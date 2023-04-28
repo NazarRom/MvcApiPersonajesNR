bb610b33c7906c666b693adb1631a1c6e9ed6b59
NazarRom <nazarrom321@gmail.com>
2023-04-28 13:35:18 +0200

    Add project files.

---
+++ MvcApiPersonajesNR/Controllers/PersonajesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiPersonajesNR.Models;
+using MvcApiPersonajesNR.Services;
+
+namespace MvcApiPersonajesNR.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        private ServicePersonajes service;
+
+        public PersonajesController(ServicePersonajes service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Personaje> personajes = await this.service.GetPersonajesAsync();
+            return View(personajes);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            Personaje personaje = await this.service.FindPersonajeAsync(id);
+            return View(personaje);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Personaje personaje)
+        {
+            await this.service.InsertPersonajeAsync(personaje.Nombre, personaje.Imagen, personaje.IdSerie);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            Personaje personaje = await this.service.FindPersonajeAsync(id);
+            return View(personaje);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Personaje personaje)
+        {
+            await this.service.UpdatePersonajeAsync(personaje.IdPersonaje, personaje.Nombre, personaje.Imagen, personaje.IdSerie);
+            return RedirectToAction("Index");
+        }
+
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await this.service.DeletePersonajeAsync(id);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ MvcApiPersonajesNR/Services/ServicePersonajes.cs
@@ -0,0 +1,108 @@
+﻿using MvcApiPersonajesNR.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MvcApiPersonajesNR.Services
+{
+    public class ServicePersonajes
+    {
+        private MediaTypeWithQualityHeaderValue Header;
+        private string UrlApi;
+
+        public ServicePersonajes(IConfiguration configuration)
+        {
+            this.Header = new MediaTypeWithQualityHeaderValue("application/json");
+            this.UrlApi = configuration.GetValue<string>("ApiUrl:ApiPersonajes");
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response = await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default;
+                }
+            }
+        }
+
+        public async Task<List<Personaje>> GetPersonajesAsync()
+        {
+            string request = "api/personajes";
+            List<Personaje> personajes = await this.CallApiAsync<List<Personaje>>(request);
+            return personajes;
+        }
+
+        public async Task<Personaje> FindPersonajeAsync(int id)
+        {
+            string request = "api/personajes/" + id;
+            Personaje personaje = await this.CallApiAsync<Personaje>(request);
+            return personaje;
+        }
+
+        public async Task InsertPersonajeAsync(string nombre, string imagen, int idserie)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/personajes";
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+
+                Personaje personaje = new Personaje
+                {
+                    Nombre = nombre,
+                    Imagen = imagen,
+                    IdSerie = idserie
+                };
+                string json = JsonConvert.SerializeObject(personaje);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(request, content);
+            }
+        }
+
+        public async Task UpdatePersonajeAsync(int idpersonaje, string nombre, string imagen, int idserie)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/personajes";
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+
+                Personaje personaje = new Personaje
+                {
+                    IdPersonaje = idpersonaje,
+                    Nombre = nombre,
+                    Imagen = imagen,
+                    IdSerie = idserie
+                };
+                string json = JsonConvert.SerializeObject(personaje);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync(request, content);
+            }
+        }
+
+        public async Task DeletePersonajeAsync(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/personajes/" + id;
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                HttpResponseMessage response = await client.DeleteAsync(request);
+            }
+        }
+
+    }
+}
